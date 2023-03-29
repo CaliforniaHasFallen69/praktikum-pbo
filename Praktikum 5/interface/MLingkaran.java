b5dd934103a9f9f3551cbab0f1005f9c01ee9749
CaliforniaHasFallen69 <100180493+CaliforniaHasFallen69@users.noreply.github.com>
2023-03-29 22:24:36 +0700

    Add files via upload

---
+++ Praktikum 5/interface/MLingkaran.java
@@ -0,0 +1,20 @@
+/*
+ * Nama File    : BangunDatar.java
+ * Nama Pembuat : Zaeri Haikal Rabbani
+ * NIM          : 24060121140159
+ * Deskripsi    : File implementasi cara mengihtung luas lingkaran
+ * Tanggal      : 27 Maret 2023
+ */
+
+ import java.util.Scanner;
+
+ public class MLingkaran {
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         System.out.print("Masukan panjang jari-jari lingkaran : ");
+         double jari = scan.nextDouble();
+         Lingkaran lr = new Lingkaran(jari);
+         System.out.println("Luas lingkaran dengan jejari " + jari + " satuan adalah : " + lr.hitungLuas());
+     }
+ }
+ 
